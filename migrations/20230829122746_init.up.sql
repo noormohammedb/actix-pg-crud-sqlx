c5b2485a425d2f9c1dcc1f4c1574be8f1da96bb3
Noor Mohammed <noormohammedb@protonmail.com>
2023-08-29 16:38:48 +0400

    migration file

--- migrations/20230829122746_init.up.sql
+++ migrations/20230829122746_init.up.sql
@@ -1,11 +1,11 @@
--- Add migration script here
+-- Add up migration script here
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE
     IF NOT EXISTS notes (
         id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
-        title VARCHAR(255) NOT NULL UNIQUE,
+        title VARCHAR(255) NOT NULL UNIQUE, 
         content TEXT NOT NULL,
         category VARCHAR(100),
         published BOOLEAN DEFAULT FALSE,
